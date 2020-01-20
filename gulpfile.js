eb51a67dc37b2848dd5a69a2bdf3c0c25c5b93e3
Yordan Hristov (Dantcho) <16123411+DantchoLV9@users.noreply.github.com>
2020-01-20 00:46:38 +0000

    Added CSS Minifier - Gulp File

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
+let cleanCSS = require("gulp-clean-css");
 
 gulp.task("less", function(cb) {
 	gulp
 		.src("./less/style.less")
 		.pipe(less())
+		.pipe(
+			cleanCSS({ debug: true, compatibility: "ie8" }, details => {
+				console.log(`${details.name}: ${details.stats.originalSize}`);
+				console.log(`${details.name}: ${details.stats.minifiedSize}`);
+			})
+		)
 		.pipe(gulp.dest("./css"));
 	cb();
 });
