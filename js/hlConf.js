b86e077e03d2176db0d5a686d9dd0e98ab646dcc
Yordan Hristov (Dantcho) <16123411+DantchoLV9@users.noreply.github.com>
2020-01-22 01:51:02 +0000

    Completed Article Elements Styles

---
+++ js/hlConf.js
@@ -0,0 +1,8 @@
+hljs.configure({
+	tabReplace: "  "
+});
+document.addEventListener("DOMContentLoaded", event => {
+	document.querySelectorAll("div code").forEach(block => {
+		hljs.highlightBlock(block);
+	});
+});
